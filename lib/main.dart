f8b7a93504c4b4a9f6256f542d1d2724e8817643
Lunalyna2 <ynachloe.venegas-24@cpu.edu.ph>
2025-05-06 15:00:41 +0800

    ara na?

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:gamify/login_screen.dart'; //import the Login page
-import 'package:gamify/selected_screen.dart'; //import the enum that tells us what screen is selected
-import 'package:gamify/signup_screen.dart'; //import the Signup page
-import 'package:gamify/splash_screen.dart'; //import the Splash page (the first screen)
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  //start gamify and show the first screen
+import 'login_screen.dart';
+import 'splash_screen.dart';
+import 'supabase_config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseApiKey,
+  );
   runApp(const Gamify());
 }
 
 class Gamify extends StatelessWidget {
-  //constructor
   const Gamify({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //main setup
     return MaterialApp(
-      debugShowCheckedModeBanner:
-          false, //removes the debug banner in the upper right corner
-      initialRoute: '/', //first screen (the SplashScreen)
-      //defines all the possible screens/routes in the app
-      routes: {
-        '/': (context) => const SplashScreen(), //SplashScreen is shown first
-        '/login':
-            (context) => const LoginPage(
-              selectedPage:
-                  SelectedPage.login, //we're passing the login page enum value
-            ),
-        '/signup':
-            (context) => const SignupPage(
-              selectedPage:
-                  SelectedPage
-                      .signup, //we're passing the signup page enum value
-            ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (_) => const SplashScreen(),
+            );
+          case '/login':
+            return MaterialPageRoute(
+              builder: (_) => const LoginPage(mode: 'login', selectedPage: null,),
+            );
+          case '/signup':
+            return MaterialPageRoute(
+              builder: (_) => const LoginPage(mode: 'signup', selectedPage: null,),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (_) => const Scaffold(
+                body: Center(
+                  child: Text('Page not found'),
+                ),
+              ),
+            );
+        }
       },
     );
   }
